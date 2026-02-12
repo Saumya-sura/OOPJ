22ccbc004ebf7f383bec4db4927c8c74e15bc64f
Saumya-sura <saumya.sura74@nmims.in>
2026-02-12 06:32:39 +0530

    lab 2 finish

--- m1/2/palindrome.java
+++ m1/2/palindrome.java
@@ -1,5 +1,19 @@
-package m1.2;
-
-public class palindrome {
-    
+import java.util.Scanner;
+class palindrome{
+    public static void main (String [] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int n= sc.nextInt();
+            int og=n,rev=0;
+            while(n!=0){
+                rev = rev*10+ n%10;
+                n/=10;
+            }    
+            if(og==rev){
+                System.out.println(og + " is a palindrome number.");
+            }
+            else{
+                System.out.println(og + " is not a palindrome number.");
+            }
+    }
 }
